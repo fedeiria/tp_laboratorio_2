a132e4f58026a67c3dba4d0b80fc411f0a326bc4
fedeiria <fedeiria@gmail.com>
2019-06-09 19:01:40 -0300

    TP4

---
+++ TP4/Iriarte.Federico.2D.TP4/TestUnitarios/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// Verifica que la lista de paquetes del Correo este instanciada.
+        /// </summary>
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Correo correo = new Correo();
+            Assert.IsNotNull(correo.Paquetes);
+        }
+    }
+}
---
+++ TP4/Iriarte.Federico.2D.TP4/TestUnitarios/UnitTest2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        /// <summary>
+        /// Verifica que no se puedan cargar dos paquetes con el mismo trackingID.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(TrackingIdRepetidoException))]
+        public void TestMethod1()
+        {
+            Correo correo = new Correo();
+            Paquete p1 = new Paquete("Matheu 645", "000-000-0001");
+            Paquete p2 = new Paquete("Matheu 645", "000-000-0001");
+
+            correo += p1;
+            correo += p2;
+            Assert.Fail();
+        }
+    }
+}
